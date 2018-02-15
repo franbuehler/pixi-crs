5f0a4d061da948d77a3b2f299e5b5fc4c0d033e7
franbuehler <franbuehler@gmail.com>
2018-02-15 21:58:34 +0000

    tests

---
+++ testcafe/tests/register.js
@@ -0,0 +1,12 @@
+import { Selector } from 'testcafe';
+import tether from 'tether';
+
+fixture`Getting Started`
+    .page`http://172.17.0.1:8000/register`;
+
+test('My first test', async t => {
+	    await t
+	        .typeText('input#email', 'testuser@pixi.owas')
+	        .typeText('input#password', 'testpw')
+	        .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+});
