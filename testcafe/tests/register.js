e30d0a34101240c358846d7943896e0bdc6f8964
franbuehler <franbuehler@gmail.com>
2018-02-18 17:02:12 +0000

    tests

--- testcafe/tests/register.js
+++ testcafe/tests/register.js
@@ -3,9 +3,9 @@ import { Selector } from 'testcafe';
 fixture`Getting Started`
     .page `http://172.17.0.1:8000/register`;
 
-test('My first test', async t => {
+test('Test DevSlop App', async t => {
 	    await t
-	        .typeText('input#email', 'testuser@pixi.owas')
+	        .typeText('input#email', 'testuser@pixi.owasp')
 	        .typeText('input#password', 'testpw')
 	        .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
 });
