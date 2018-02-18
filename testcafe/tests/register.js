68fd8912ab5d5cac435edcf6892ecb232b30dbf4
franbuehler <franbuehler@gmail.com>
2018-02-18 17:11:29 +0000

    tests

--- testcafe/tests/register.js
+++ testcafe/tests/register.js
@@ -1,7 +1,7 @@
 import { Selector } from 'testcafe';
 
 fixture`Getting Started`
-    .page `http://172.17.0.1:8000/register`;
+    .page `http://172.17.0.3:8000/register`;
 
 test('Test DevSlop App', async t => {
 	    await t
