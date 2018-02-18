94122dca6c2521fd5b31989ae017e578ccd34088
franbuehler <franbuehler@gmail.com>
2018-02-18 21:00:05 +0000

    tests

--- testcafe/tests/register.js
+++ testcafe/tests/register.js
@@ -7,5 +7,5 @@ test('Test DevSlop App', async t => {
 	    await t
 	      //  .typeText('#email', 'testuser@pixi.owasp')
 	      //  .typeText('input#password', 'testpw')
-	        .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+	        this.t.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
 });
