bc29b808597b026f9e158950a27fefa66e136cc0
franbuehler <franbuehler@gmail.com>
2018-03-09 21:33:23 +0000

    added input before selector

--- testcafe/tests/mongodb.js
+++ testcafe/tests/mongodb.js
@@ -6,8 +6,8 @@ fixture`Getting Started`
 test('Register User', async t => {
     await t
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
-        .typeText('#email', 'testuser@pixi.owasp')
-        .typeText('#password', 'testpw')
+        .typeText('input#email', 'testuser@pixi.owasp')
+        .typeText('input#password', 'testpw')
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
         //After registration the search pixi field should be there
         //.expect('#search_query')
@@ -15,8 +15,8 @@ test('Register User', async t => {
 
 test('Login and Search', async t => {
     await t
-        .typeText('#user', 'testuser@pixi.owasp')
-        .typeText('#pass', 'testpw')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
         //After login we search for Lunch
         .typeText('#search_query', 'true, $where: \'1 == 1\'')
         .pressKey('enter')
--- testcafe/tests/mongodbcrs.js
+++ testcafe/tests/mongodbcrs.js
@@ -6,8 +6,8 @@ fixture`Getting Started`
 test('Register User WITH CRS', async t => {
     await t
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
-        .typeText('#email', 'testuser@pixi.owasp')
-        .typeText('#password', 'testpw')
+        .typeText('input#email', 'testuser@pixi.owasp')
+        .typeText('input#password', 'testpw')
         .click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
         //After registration the search pixi field should be there
         //.expect('#search_query')
@@ -15,8 +15,8 @@ test('Register User WITH CRS', async t => {
 
 test('Login and Search', async t => {
     await t
-        .typeText('#user', 'testuser@pixi.owasp')
-        .typeText('#pass', 'testpw')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
         //After login we search for Lunch
         .typeText('#search_query', '[$exists]')
         .pressKey('enter')
