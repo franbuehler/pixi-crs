430613d6ca327224657bd9f58d6f562d356f6fa8
franbuehler <franbuehler@gmail.com>
2018-04-26 09:15:58 +0000

    extend testcafe tests

--- testcafe/tests/test.js
+++ testcafe/tests/test.js
@@ -31,3 +31,12 @@ test('Search String', async t => {
         .pressKey('enter')
         //.expect('body > div > div:nth-child(12) > div > div.card-deck')
 });
+
+test('Logout User', async t => {
+    await t
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-4 > a:nth-child(3) > button')
+        .typeText('input#user', 'testuser@pixi.owasp')
+        .typeText('input#pass', 'testpw')
+	.click('body > div.jumbotron.jumbotron-fluid > div > div:nth-child(5) > div.col-8 > form > button')
+	.click('body > div > div:nth-child(1) > div.col-md-7 > ul > li:nth-child(4) > a')
+});
